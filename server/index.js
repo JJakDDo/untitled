f0eb0a95c824910f1719ca373f8dcc2d133a6ebf
JJakDDo <kangdw124@gmail.com>
2022-04-25 23:53:59 +0900

    Feat: start server and connect to DB

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./db/connect");
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/untitled";
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await connectDB(MONGODB_URI);
+    console.log(`DB is connected to ${MONGODB_URI}`);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on PORT ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
